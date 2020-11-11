00da8646870a75cbf8701e77871abf412c28a520
DhaeseThibault <55786808+DhaeseThibault@users.noreply.github.com>
2020-11-11 15:45:53 +0100

    Javascript Recent weather

--- script/app.js
+++ script/app.js
@@ -3,13 +3,17 @@ const apiKey = "elQ8cNca9AWnuQLHJQTrKNJfku0KvV88CknTTazw"
 
 let showResult = queryResponse => {
     console.log({queryResponse});
+    const latest_sol = queryResponse.sol_keys[6];
+    const getal = 123.52346;
+    console.log(getal.toFixed(1));
+
 
     // We gaan eerst een paar onderdelen opvullen
     // Zorg dat de SOL wordt weergegeven
-    document.querySelector(".js-SOLnow").innerHTML += `${queryResponse.sol_keys[3]}`
-    document.querySelector(".js-sol1").innerHTML = `SOL: ${queryResponse.sol_keys[0]}<br> Temp: ${queryResponse[689].AT.av}`;
-    document.querySelector(".js-sol2").innerHTML = `SOL: ${queryResponse.sol_keys[1]}<br> Temp: ${queryResponse[690].AT.av}`;
-    document.querySelector(".js-sol3").innerHTML = `SOL: ${queryResponse.sol_keys[2]}<br> Temp: ${queryResponse[691].AT.av}`;
+    document.querySelector(".js-SOLnow").innerHTML += `${queryResponse.sol_keys[6]}`;
+    document.querySelector(".js-SOLnowGemTemp").innerHTML += `${(queryResponse[latest_sol].AT.av).toFixed(1)}°F`;
+    document.querySelector(".js-SOLnowMinTemp").innerHTML += `${(queryResponse[latest_sol].AT.mn).toFixed(1)}°F`;
+    document.querySelector(".js-SOLnowMaxTemp").innerHTML += `${(queryResponse[latest_sol].AT.mx).toFixed(1)}°F`;
 
 };
 
@@ -17,15 +21,15 @@ let showResult = queryResponse => {
 // 2 Aan de hand van de url gaan we de API ophalen
 const getApi = async () => {
     // Eerst bouwen we onze url op
-    let url = `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0&sol_id=684`
+    let url = `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0&sol_id=684`;
 
     // Met de fetch API proberen we de data op te halen
     const data = await fetch(url)
     .then((res) => res.json())
-    .catch(err => console.error(err))
+    .catch(err => console.error(err));
 
     // console.log(data)
-    showResult(data)
+    showResult(data);
 };
 
 document.addEventListener("DOMContentLoaded", function(){
